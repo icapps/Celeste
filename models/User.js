8b1dd24d5da379c7e491029cd5ac7ca70794b06c
Maarten Anckaert <maarten.anckaert@icapps.com>
2016-05-13 22:07:29 +0200

    Updated model to be slack-integration ready

--- models/User.js
+++ models/User.js
@@ -8,11 +8,31 @@ var Types = keystone.Field.Types;
 var User = new keystone.List('User');
 
 User.add({
-	name: { type: Types.Name, required: true, index: true },
-	email: { type: Types.Email, initial: true, required: true, index: true },
-	password: { type: Types.Password, initial: true, required: true },
+	//Standard keystone props
+	name: {type: Types.Name, required: false, index: true},
+	email: {type: Types.Email, initial: true, required: false, index: true},
+	password: {type: Types.Password, initial: true, required: false},
+
+	//Slack props
+	slack_id: {type:String, initial:false, required: false, noedit:true},
+	team_id:{type:String, initial:false, required: false, noedit:true},
+	deleted:{type:Boolean, initial:false, required: false, noedit:true},
+	status:{type:String, initial:false, required: false, noedit:true},
+	color:{type:String, initial:false, required: false, noedit:true},
+	real_name:{type:String, initial:false, required: false, noedit:true},
+	profile:{
+		first_name: {type:String, initial:false, required: false, noedit:true},
+		last_name: {type:String, initial:false, required: false, noedit:true},
+		title: {type:String, initial:false, required: false, noedit:true},
+		skype: {type:String, initial:false, required: false, noedit:true},
+		phone: {type:String, initial:false, required: false, noedit:true},
+		image: {type:String, initial:false, required: false, noedit:true}
+	}
+	
 }, 'Permissions', {
-	isAdmin: { type: Boolean, label: 'Can access Keystone', index: true },
+	isAdmin: {type: Boolean, label: 'Can access Keystone', index: true},
+
+	
 });
 
 // Provide access to Keystone
