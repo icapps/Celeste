160a89d1b13b0e138c3681a083c6f44844fd42a9
Mo Binni <mo.binni@hotmail.com>
2016-05-14 18:17:03 +0200

    fixes

--- routes/api/comments.js
+++ routes/api/comments.js
@@ -12,30 +12,31 @@ module.exports.getUserComments = function (req, res) {
 	};
 	rp(options)
 		.then(function (result) {
-			var promises = [];
-			result.messages.forEach(function (message, i) {
-				var options = {
-					uri: 'https://slack.com/api/users.info?',
-					qs: {
-						token: process.env.SLACK_TOKEN,
-						user: message.user
-					},
-					json: true
-				};
-				var promise = rp(options);
-				promises.push(promise);
-			});
-
-			Promise.all(promises).then(function (result2) {
-				// TODO: possible that the values don't map to the correct user HYPE
-				var results = result.messages.map(function (message, i) {
-					var finalMessage = message;
-					finalMessage.user = result2[i].user;
-					return finalMessage;
+			if (result && result.length > 0) {
+				var promises = [];
+				result.messages.forEach(function (message, i) {
+					var options = {
+						uri: 'https://slack.com/api/users.info?',
+						qs: {
+							token: process.env.SLACK_TOKEN,
+							user: message.user
+						},
+						json: true
+					};
+					var promise = rp(options);
+					promises.push(promise);
 				});
-				res.send(results);
-			})
-			
+
+				Promise.all(promises).then(function (result2) {
+					// TODO: possible that the values don't map to the correct user HYPE
+					var results = result.messages.map(function (message, i) {
+						var finalMessage = message;
+						finalMessage.user = result2[i].user;
+						return finalMessage;
+					});
+					res.send(results);
+				})
+			}
 		});
-	
+
 }
